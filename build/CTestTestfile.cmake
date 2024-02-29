1299c97cd92c2e2c538fffcffd58b130b880fae2
Furkan Yavuz <furkanyavuz0926@gmail.com>
2024-02-29 15:37:59 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/furkan/Desktop/cmakeQuickStart
-# Build directory: /home/furkan/Desktop/cmakeQuickStart/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
