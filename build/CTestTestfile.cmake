323e373adcdfeb9ebeda1b28d4f044fdc8feeecf
Furkan Yavuz <furkanyavuz0926@gmail.com>
2024-03-05 14:52:47 +0300

    deleted build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/furkan/Desktop/Multiclient-Server-Secure-Chat-Application
-# Build directory: /home/furkan/Desktop/Multiclient-Server-Secure-Chat-Application/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
